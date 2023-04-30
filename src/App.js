b5b16b4a66b3fbfc626d134898f93533bd7ada1a
1309042 <1309042@naver.com>
2023-05-01 08:24:15 +0900

    Link 추가
    Movie.js에 제목 클릭 시 디테일뷰로 이동하는 Link to 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 function App() {
   return (
     <Router>
+      <Switch>
         <Route path="/movie">
           <Detail />
         </Route>
-      <Switch>
         <Route path="/">
           <Home />
         </Route>
